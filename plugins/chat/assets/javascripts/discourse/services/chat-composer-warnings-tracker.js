42c122266eac034ecd48643619dfb5af4beb8994
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-08-28 20:11:03 +0400

    Extract #resetMentionStats – another usage

--- plugins/chat/assets/javascripts/discourse/services/chat-composer-warnings-tracker.js
+++ plugins/chat/assets/javascripts/discourse/services/chat-composer-warnings-tracker.js
@@ -34,11 +34,7 @@ export default class ChatComposerWarningsTracker extends Service {
 
   @bind
   reset() {
-    this.unreachableGroupMentions = [];
-    this.unreachableGroupMentions = [];
-    this.overMembersLimitGroupMentions = [];
-    this.tooManyMentions = false;
-    this.channelWideMentionDisallowed = false;
+    this.#resetMentionStats();
     this.mentionsCount = 0;
     cancel(this.mentionsTimer);
   }
