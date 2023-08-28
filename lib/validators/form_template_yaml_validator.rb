048115a9246ad0369d39ab484bc5a9ebbb70aed1
Renato Atilio <renato@discourse.org>
2023-08-28 17:50:50 -0300

    FEATURE: adds support for initial values through /new-topic to form templates

--- lib/validators/form_template_yaml_validator.rb
+++ lib/validators/form_template_yaml_validator.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+RESERVED_KEYWORDS = %w[title body category category_id tags]
+
 class FormTemplateYamlValidator < ActiveModel::Validator
   def validate(record)
     begin
       yaml = Psych.safe_load(record.template)
-      check_missing_type(record, yaml)
+      check_missing_fields(record, yaml)
       check_allowed_types(record, yaml)
+      check_ids(record, yaml)
     rescue Psych::SyntaxError
       record.errors.add(:template, I18n.t("form_templates.errors.invalid_yaml"))
     end
@@ -29,11 +32,33 @@ class FormTemplateYamlValidator < ActiveModel::Validator
     end
   end
 
-  def check_missing_type(record, yaml)
+  def check_missing_fields(record, yaml)
     yaml.each do |field|
       if field["type"].blank?
-        return record.errors.add(:template, I18n.t("form_templates.errors.missing_type"))
+        return(record.errors.add(:template, I18n.t("form_templates.errors.missing_type")))
       end
+      if field["id"].blank?
+        return(record.errors.add(:template, I18n.t("form_templates.errors.missing_id")))
+      end
+    end
+  end
+
+  def check_ids(record, yaml)
+    ids = []
+    yaml.each do |field|
+      next if field["id"].blank?
+
+      if RESERVED_KEYWORDS.include?(field["id"])
+        return(
+          record.errors.add(:template, I18n.t("form_templates.errors.reserved_id", id: field["id"]))
+        )
+      end
+
+      if ids.include?(field["id"])
+        return(record.errors.add(:template, I18n.t("form_templates.errors.duplicate_ids")))
+      end
+
+      ids << field["id"]
     end
   end
 end
